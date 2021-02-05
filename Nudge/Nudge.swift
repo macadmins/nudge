a868d55572e4c15036b64bce792fe81081f99324
Erik Gomez <erikg@uber.com>
2021-02-05 08:07:05 -0600

    move nudgeprefs to its own swift file

--- Nudge/Nudge.swift
+++ Nudge/Nudge.swift
@@ -7,10 +7,8 @@
 
 import SwiftUI
 
-// JSON
-// TODO move loadnudgeprefs to it's own swift file
-// TODO: use CFPreferences to get mdm/mobileconfig logic and prioritize over json
-let nudge_prefs = loadNudgePrefs()
+// Prefs
+let nudge_prefs = nudgePrefs().loadNudgePrefs()
 
 // All of this image stuff needs to be refactored.
 // Setup Variables for light logo
@@ -351,67 +349,6 @@ struct Nudge: View {
     }
 
 }
-
-// Function stuff
-
-// Setup our json structure
-struct jsonPrefs: Codable {
-    let button_title_text: String
-    let button_sub_titletext: String
-    let cut_off_date: String
-    let cut_off_date_warning: Int
-    let days_between_notifications: Int
-    let dismissal_count_threshold: Int
-    let logo_dark_path: String
-    let logo_light_path: String
-    let main_subtitle_text: String
-    let main_title_text: String
-    let minimum_os_sub_build_version: String
-    let minimum_os_version: String
-    let more_info_url: String
-    let no_timer: Bool
-    let paragraph_text: String
-    let paragraph_title_text: String
-    let path_to_app: String
-    let random_delay: Bool
-    let screenshot_path: String
-    let timer_day_1: Int
-    let timer_day_3: Int
-    let timer_elapsed: Int
-    let timer_final: Int
-    let timer_initial: Int
-    let update_minor: Bool
-    let update_minor_days: Int
-}
-
-func decode(data: Data) throws -> jsonPrefs? {
-    do {
-        let decoder = JSONDecoder()
-        let data = try decoder.decode(jsonPrefs.self, from: data)
-        return data
-    } catch let error {
-        print(error)
-        return nil
-    }
-}
-
-func loadNudgePrefs() -> jsonPrefs? {
-    guard let fileURL = Bundle.main.url(forResource: "example", withExtension: "json") else {
-        print("couldn't find the file")
-        return nil
-    }
-    
-    do {
-        let content = try Data(contentsOf: fileURL)
-        let user = try decode(data: content)
-        return user
-
-    } catch let error {
-        print(error)
-        return nil
-    }
-}
-
 
 struct Login_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Nudge/nudgePrefs.swift
@@ -0,0 +1,69 @@
+//
+//  nudgePrefs.swift
+//  Nudge
+//
+//  Created by Erik Gomez on 2/5/21.
+//
+
+import Foundation
+
+// TODO: use CFPreferences to get mdm/mobileconfig logic and prioritize over json
+
+struct nudgePrefs{
+    func decode(data: Data) throws -> jsonPrefs? {
+        do {
+            let decoder = JSONDecoder()
+            let data = try decoder.decode(jsonPrefs.self, from: data)
+            return data
+        } catch let error {
+            print(error)
+            return nil
+        }
+    }
+
+    func loadNudgePrefs() -> jsonPrefs? {
+        guard let fileURL = Bundle.main.url(forResource: "example", withExtension: "json") else {
+            print("couldn't find the file")
+            return nil
+        }
+        
+        do {
+            let content = try Data(contentsOf: fileURL)
+            let user = try decode(data: content)
+            return user
+
+        } catch let error {
+            print(error)
+            return nil
+        }
+    }
+}
+
+struct jsonPrefs: Codable {
+    let button_title_text: String
+    let button_sub_titletext: String
+    let cut_off_date: String
+    let cut_off_date_warning: Int
+    let days_between_notifications: Int
+    let dismissal_count_threshold: Int
+    let logo_dark_path: String
+    let logo_light_path: String
+    let main_subtitle_text: String
+    let main_title_text: String
+    let minimum_os_sub_build_version: String
+    let minimum_os_version: String
+    let more_info_url: String
+    let no_timer: Bool
+    let paragraph_text: String
+    let paragraph_title_text: String
+    let path_to_app: String
+    let random_delay: Bool
+    let screenshot_path: String
+    let timer_day_1: Int
+    let timer_day_3: Int
+    let timer_elapsed: Int
+    let timer_final: Int
+    let timer_initial: Int
+    let update_minor: Bool
+    let update_minor_days: Int
+}
