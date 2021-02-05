2266dfe4fd0eb9b26d0122c98b31fbba905d734e
Erik Gomez <erikg@uber.com>
2021-02-05 07:12:59 -0600

    Hide min max and close buttons

--- Nudge/ContentView.swift
+++ Nudge/ContentView.swift
@@ -7,8 +7,16 @@
 
 import SwiftUI
 
+// https://stackoverflow.com/a/66039864
+// https://gist.github.com/steve228uk/c960b4880480c6ed186d
+
 struct ContentView: View {
     var body: some View {
+        HostingWindowFinder {window in
+            window?.standardWindowButton(.closeButton)?.isHidden = true //hides the red close button
+            window?.standardWindowButton(.miniaturizeButton)?.isHidden = true //hides the yellow miniaturize button
+            window?.standardWindowButton(.zoomButton)?.isHidden = true //this removes the green zoom button
+        }
         Nudge()
     }
 }
@@ -17,4 +25,17 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct HostingWindowFinder: NSViewRepresentable {
+    var callback: (NSWindow?) -> ()
+
+    func makeNSView(context: Self.Context) -> NSView {
+        let view = NSView()
+        DispatchQueue.main.async { [weak view] in
+            self.callback(view?.window)
+        }
+        return view
+    }
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
