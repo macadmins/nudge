33c650d81d2a44c6ae3473abb634f35dc460df8f
Erik Gomez <e@eriknicolasgomez.com>
2021-02-10 22:47:50 -0600

    code identity is slightly different in GHA

--- build_nudge.zsh
+++ build_nudge.zsh
@@ -21,7 +21,7 @@ fi
 
 # build nudge
 echo "Building Nudge"
-/Applications/Xcode_12.4.app/Contents/Developer/usr/bin/xcodebuild -project Nudge.xcodeproj "CODE_SIGN_IDENTITY=Apple Distribution: Clever DevOps Co. (9GQZ7KUFR6)"
+/Applications/Xcode_12.4.app/Contents/Developer/usr/bin/xcodebuild -project Nudge.xcodeproj CODE_SIGN_IDENTITY="Apple Distribution: Clever DevOps Co."
 
 XCB_RESULT="$?"
 if [ "${XCB_RESULT}" != "0" ]; then
