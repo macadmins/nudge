c7fde0854dac037b01716dd52ab5bb65ccd7b00b
Erik Gomez <e@eriknicolasgomez.com>
2021-02-10 22:44:26 -0600

    move back to manual code signing and specify the cert name

--- build_nudge.zsh
+++ build_nudge.zsh
@@ -21,7 +21,7 @@ fi
 
 # build nudge
 echo "Building Nudge"
-/usr/bin/xcodebuild -project Nudge.xcodeproj
+/Applications/Xcode_12.4.app/Contents/Developer/usr/bin/xcodebuild -project Nudge.xcodeproj "CODE_SIGN_IDENTITY=Apple Distribution: Clever DevOps Co. (9GQZ7KUFR6)"
 
 XCB_RESULT="$?"
 if [ "${XCB_RESULT}" != "0" ]; then
