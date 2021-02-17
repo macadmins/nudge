f0a9d02ebfdf0be548dbf0cda0c0205434768784
Erik Gomez <erikg@uber.com>
2021-02-17 14:11:00 -0600

    add another note

--- Nudge/defaults.swift
+++ Nudge/defaults.swift
@@ -56,6 +56,7 @@ func getOSVersionRequirementsJSON() -> OSVersionRequirement? {
     return nil
 }
 
+// Function to compare current language against the available updateURLs
 func getUpdateURL() -> String? {
     if Utils().demoModeEnabled() {
         return "https://support.apple.com/en-us/HT201541"
