c5c1dd4e5c0924c9e7463e65419c33ad4bc5b855
Erik Gomez <erikg@uber.com>
2024-05-21 17:14:37 -0500

    add another todo

--- Nudge/UI/Main.swift
+++ Nudge/UI/Main.swift
@@ -179,6 +179,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // TODO: Add localization for "unsupported devices" text fields
         // TODO: Get someone to update JAMF JSON schema for all the new keys and wiki
         // TODO: add support for custom sofa feed url
+        // TODO: check the sofa json etag even after the timelimit age to reduce more bandwidth
         if OptionalFeatureVariables.utilizeSOFAFeed {
             if let macOSSOFAAssets = Globals.sofaAssets?.osVersions {
                 var foundMatch = false
