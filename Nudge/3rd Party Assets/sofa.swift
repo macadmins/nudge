585712feeb19c03cd2f8fa78ca25af1ccad912a8
Erik Gomez <erikg@uber.com>
2024-05-24 13:14:53 -0500

    move todos

--- Nudge/3rd Party Assets/sofa.swift
+++ Nudge/3rd Party Assets/sofa.swift
@@ -231,6 +231,7 @@ class SOFA: NSObject, URLSessionDelegate {
         let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
         request.addValue("\(Globals.bundleID)/\(VersionManager.getNudgeVersion())", forHTTPHeaderField: "User-Agent")
         request.setValue(lastEtag, forHTTPHeaderField: "If-None-Match")
+        // TODO: I'm saving the Etag and sending it, but due to forcing this into a syncronous call, it is always returning a 200 code. When using this in an asycronous method, it eventually returns the 304 response. I'm not sure how to fix this bug.
         var attempts = 0
 
         var responseData: Data?
